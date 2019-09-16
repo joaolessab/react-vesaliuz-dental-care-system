20f2e593318d731176568f2ce860f4c5203cb296
João Vitor Lessa <joaovitorlessa@gmail.com>
2019-09-16 18:56:56 -0300

    Aplicação base

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,9 @@
+// React Library
 import React from 'react';
+
+// Reacty Library para manipular o DOM - (Árvore de Elementos)
 import ReactDOM from 'react-dom';
-import './index.css';
+
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
