40fb3f48bb053b88677201b7f337daec5d996b97
João Vitor Lessa <joaovitorlessa@gmail.com>
2019-10-07 23:47:34 -0300

    [Redirecionamento]{Front} - Redirecionando rotas simples, com o history

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 //Arquivos CSS e Imagens devem ser importados aqui
 import './Login.css';
 import logo from '../assets/logo.svg';
 
-export default function Login(){
+export default function Login({ history }){ // React herdou a propriedade history dos routes
+    // Iniciando useStates
+    const [username, setUsername] = useState('');
+
+    // Declarando uma função básica de preenchimento automático
+    function handleSubmit(e){
+        e.preventDefault();
+        console.log(username);
+
+        // Redireciona a rota
+        history.push('/main');
+    }
+
     return (
         <div className="login-container">
-            <form>
+            <form onSubmit = { handleSubmit }>
                 <img src={logo} alt= "Vesalius"/>
                 <input 
                     placeholder="Digite seu usuario"
+                    value = {username}
+                    onChange = { e => setUsername(e.target.value) } /* Chamando useState para armazenar valor no username */
                 />
                 <input 
                     type="password"
