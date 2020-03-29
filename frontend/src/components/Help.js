0631491099121b21cbcd65eae6a051ba25f781aa
João Vitor Lessa <joaovitorlessa@gmail.com>
2020-03-29 18:16:23 -0300

    Consegui atualizar componente

--- frontend/src/components/Help.js
+++ frontend/src/components/Help.js
@@ -1,9 +1,5 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { defaults } from 'react-chartjs-2';
-import  { Line } from 'react-chartjs-2';
-import { Pie } from 'react-chartjs-2'; 
-import { Bar } from 'react-chartjs-2';
 
 // ARQUIVOS CSS E IMAGENS DEVEM SER IMPORTADOS AQUI
 import '../App.css';
@@ -11,96 +7,7 @@ import './Help.css';
 import './extraStyles/Help--Responsive.css';
 
 export default function Help(){
-    defaults.global.defaultFontFamily = 'Averta';
-
-    // CHART DATA 
-    const lineChartData = {
-        data: {
-            labels: ["Jan", "Fev", "Mar", "Abr", "Mai"],
-            datasets: [
-                {
-                    label: "Limpezas",
-                    backgroundColor: "#55BCC9",
-                    borderColor: "white",
-                    borderWidth: 2,
-                    data: [4, 5, 11, 10, 32]
-                },
-                {
-                    label: "Extrações",
-                    backgroundColor: "#97CAEF",
-                    borderColor: "white",
-                    borderWidth: 2,
-                    data: [14, 15, 1, 20, 20]
-                },
-                {
-                    label: "Clareamentos",
-                    backgroundColor: "#557A95",
-                    borderColor: "white",
-                    borderWidth: 2, 
-                    data: [24, 25, 1, 10, 40]
-                }
-            ]
-        },
-        options: {
-            legend: {
-                display: true,
-                labels: {fontFamily: 'Averta'}
-            },
-            responsive: true
-        }
-    };
-
-    const pieChartData = {
-        data: {
-            labels: ["Janeiro", "Fevereiro", "Março", "Abril", "Maio"],
-            datasets: [
-                {
-                    backgroundColor: [
-                        "#5D001E",
-                        "#E3E2DF",
-                        "#E3AFBC",
-                        "#9A1750",
-                        "#EE4C7C"
-                    ],
-                    borderColor: "white",
-                    borderWidth: 2,
-                    data: [14, 15, 4, 20, 8]
-                }
-            ]
-        },
-        options: {
-            legend: {display: false},
-            responsive: true,
-            // Se setar para false, ele respeitará o tamanho customizado definido por você
-            maintainAspectRatio: true
-        }
-    };
-
-    const barChartData = {
-        data: {
-            labels: ["Jan", "Fev", "Mar", "Abr", "Mai"],
-            datasets: [
-                {
-                    backgroundColor: [
-                        "#2C3531",
-                        "#116466",
-                        "#D9B08C",
-                        "#FFCB9A",
-                        "#D1E8E2"
-                    ],
-                    borderColor: "white",
-                    borderWidth: 2,
-                    data: [14, 15, 10, 20, 12]
-                }
-            ]
-        },
-        options: {
-            legend: {display: false},
-            responsive: true,
-            maintainAspectRatio: true
-        }
-    };
-    
+   
     // FUNCOES DO JAVASCRIPT
     function teste(e){
         debugger
@@ -108,92 +15,6 @@ export default function Help(){
 
     // RETORNO BÁSICO DO HTML
     return (
-        <div className="container--miolo">
-            <div className="container--content">
-                <div className="div--title">
-                    <h1>Página Inicial</h1>
-                </div>
-
-                {/* Auto Suggest */}
-
-                <div className="div--info">
-                    <div className="div--info-icon">
-                    </div>
-                    <div className="div--info-text">
-                        <h1>Aprenda como organizar sua agenda</h1>
-                        <p>Seus rendimentos dependem do seu tempo disponível. Quanto mais você escalar o seu negócio, mais resultados irá obter. Esse artigo aborda exatamente o passo a passo para...</p>
-                    </div>
-                </div>
-
-                <div className="div--chart">
-                    <div className="revenue">
-                        <h1>R$ 2.730,50</h1>
-                        <p>Opa, lucrou? Confira sua receita</p>
-                    </div>
-                    <div className="expense">
-                        <h1>R$ 1.302,26</h1>
-                        <p>Gastou, né? Confira suas despesas</p>
-                    </div>
-                </div>
-            </div>
-
-            <div className="container--auxiliar">
-                <div className="div--topbuttons">
-                    <button className="selected">Dia</button>
-                    <button>Semana</button>
-                    <button>Mês</button>
-                    <button>Ano</button>
-                </div>
-                
-                {/* Bigger Chart */}
-                <div className="div--line">
-                    <div className="div--titles">
-                        <p>Tratamentos</p>
-                    </div>
-                    <div className="div--chart-final">
-                        <Line
-                            options = {lineChartData.options}
-                            data = {lineChartData.data}
-                        />
-                    </div>
-                    <div className="div--details">
-                        <p>Detalhes</p>
-                        <button></button>
-                    </div>
-                </div>
-                <div className="div--chart-bottom">
-                    <div className="div--bar-left">
-                        <div className="div--titles">
-                            <p>Tratamentos por mês</p>
-                        </div>
-                        <div className="div--chart-final">
-                            <Pie 
-                                options = {pieChartData.options}
-                                data = {pieChartData.data}
-                            />
-                        </div>
-                        <div className="div--details">
-                            <p>Detalhes</p>
-                            <button></button>
-                        </div>
-                    </div>
-                    <div className="div--bar-right">
-                        <div className="div--titles">
-                            <p>Tratamentos</p>
-                        </div>
-                        <div className="div--chart-final">
-                            <Bar
-                                options = {barChartData.options}
-                                data = {barChartData.data}
-                            />
-                        </div>
-                        <div className="div--details">
-                            <p>Detalhes</p>
-                            <button></button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <p>Oi</p>
     );
 }
--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 // ARQUIVOS CSS E IMAGENS DEVEM SER IMPORTADOS AQUI
 import './Main.css';
@@ -7,156 +8,184 @@ import './extraStyles/Main--Animations.css';
 import Resume from '../components/Resume';
 import Help from '../components/Help';
 
-export default function Main(){
-    // FUNCOES DO JAVASCRIPT
-    function animateSidebar(e){
-        if (e == "expanded"){
-            //Alterando CSS
+// FUNCOES DO JAVASCRIPT
+function animateSidebar(e){
+    if (e == "expanded"){
+        //Alterando CSS
+        document.getElementsByClassName("container--sidebar")[0].classList.remove("sidebar--visible-left");
+        document.getElementsByClassName("container--sidebar")[0].classList.add("sidebar--hidden-left");            
+        document.getElementsByClassName("button--sidebar-retract")[0].classList.add("button--sidebar-expand");
+        document.getElementsByClassName("button--sidebar-retract")[0].classList.remove("button--sidebar-retract");
+    }
+    else{
+        //Retracao
+        if (e.target.classList.contains("button--sidebar-retract")){
+            //Alterando CSS            
             document.getElementsByClassName("container--sidebar")[0].classList.remove("sidebar--visible-left");
-            document.getElementsByClassName("container--sidebar")[0].classList.add("sidebar--hidden-left");            
-            document.getElementsByClassName("button--sidebar-retract")[0].classList.add("button--sidebar-expand");
-            document.getElementsByClassName("button--sidebar-retract")[0].classList.remove("button--sidebar-retract");
+            document.getElementsByClassName("container--sidebar")[0].classList.add("sidebar--hidden-left");
+            e.target.classList.remove("button--sidebar-retract");
+            e.target.classList.add("button--sidebar-expand");
         }
         else{
-            //Retracao
-            if (e.target.classList.contains("button--sidebar-retract")){
-                //Alterando CSS            
-                document.getElementsByClassName("container--sidebar")[0].classList.remove("sidebar--visible-left");
-                document.getElementsByClassName("container--sidebar")[0].classList.add("sidebar--hidden-left");
-                e.target.classList.remove("button--sidebar-retract");
-                e.target.classList.add("button--sidebar-expand");
-            }
-            else{
-                //Alterando CSS
-                document.getElementsByClassName("container--sidebar")[0].classList.remove("sidebar--hidden-left");
-                document.getElementsByClassName("container--sidebar")[0].classList.add("sidebar--visible-left");
-                e.target.classList.remove("button--sidebar-expand");
-                e.target.classList.add("button--sidebar-retract");            
-            }
+            //Alterando CSS
+            document.getElementsByClassName("container--sidebar")[0].classList.remove("sidebar--hidden-left");
+            document.getElementsByClassName("container--sidebar")[0].classList.add("sidebar--visible-left");
+            e.target.classList.remove("button--sidebar-expand");
+            e.target.classList.add("button--sidebar-retract");            
         }
-    };
+    }
+};
 
-    function accessPage(page){
-        alert(page);
-    };
+function accessPage(page){
+    debugger
+    alert(page);
+    var container = document.getElementById("dynamicContent");
+    debugger
+    ReactDOM.unmountComponentAtNode(container);
+};
 
-    window.addEventListener('load', handleResize);
-    function handleResize(){
-        if (window.outerWidth < 490){
-            animateSidebar("expanded");
-        }
-    };
+window.addEventListener('load', handleResize);
+function handleResize(){
+    if (window.outerWidth < 490){
+        animateSidebar("expanded");
+    }
+};
 
+class Main extends React.Component{
+    // Substituindo o construtor do componente (são inputs do componente)
+    constructor(props) {
+        super(props);
+        this.state = {
+            child: Resume
+        };
+
+        this.testeStr = "oi"
+
+        this.teste = function joao (oi){
+            this.testeStr = "tchau";
+            this.setState({child: Help});
+            this.forceUpdate();
+        };
+    }
+    
     // RETORNO BÁSICO DO HTML
-    return (
-        <div className="container--father">
-            <div className="container--blackboard-columnar">                               
-                
-                {/* Main Navbar */}
-                <div className="navbar-main--top">
-                    <div className="left--side">
-                        <div className="logo"></div>
-                        <p>Vesaliuz</p>
-                    </div>
+    render(){
+        return (
+            <div className="container--father">
+                <div className="container--blackboard-columnar">                               
                     
-                    <div className="right--side">
-                        <i className="alert"></i>
-                        <p>Bem-vindo, João</p>
-                        <div className="photo"></div>
-                        <button className="button--rectangle-secondary-middle-size lrbuton icon-exit--white">
-                            <p>Sair</p>
-                        </button>
-                    </div>
-                </div>
-             
-                {/* Main Content */}
-                <div className="container--main-content">
-                    
-                    {/* Sidebar */}
-                    <div className="container--sidebar">
-                        <div className="sidebar" id="box">
-                            <button 
-                                className="icon--home-white selected"                                
-                                onClick={() => accessPage("home")}
-                            >
-                                <span>Início</span>
-                            </button>
-
-                            <button 
-                                className="icon--agendamentos"
-                                onClick={() => accessPage("agenda")}
-                            >
-                                <span>Agenda</span>
-                            </button>
-
-                            <button 
-                                className="icon--pacientes"
-                                onClick={() => accessPage("pacientes")}
-                            >
-                                <span>Pacientes</span>
-                            </button>
-                            
-                            <button 
-                                className="icon--financas"
-                                onClick={() => accessPage("financas")}
-                            >
-                                <span>Financeiro</span>
-                            </button>
-                            
-                            <button 
-                                className="icon--exams"
-                                onClick={() => accessPage("exams")}
-                            >
-                                <span>Exames</span>
-                            </button>
-
-                            <button 
-                                className="icon--procedure"
-                                onClick={() => accessPage("tratamentos")}
-                            >       
-                                <span>Tratamentos</span>
-                            </button>
-                            
-                            <button 
-                                className="icon--laboratory"
-                                onClick={() => accessPage("laboratorio")}
-                            >
-                                <span>Laboratório</span>
-                            </button>
-                            
-                            <button 
-                                className="icon--news"
-                                onClick={() => accessPage("noticias")}
-                            >
-                                <span>Notícias</span>
-                            </button>                            
-
-                            <button 
-                                className="icon--help"
-                                onClick={() => accessPage("ajuda")}
-                            >
-                                <span>Ajuda</span>
-                            </button>
-                            
-                            <button 
-                                className="icon--nubibuz"
-                                onClick={() => accessPage("nubibuz")}
-                            >
-                                <span>Sobre Nós</span>
-                            </button>                   
+                    {/* Main Navbar */}
+                    <div className="navbar-main--top">
+                        <div className="left--side">
+                            <div className="logo"></div>
+                            <p>Vesaliuz</p>
                         </div>
-                    </div>        
+                        
+                        <div className="right--side">
+                            <i className="alert"></i>
+                            <p>Bem-vindo, João</p>
+                            <div className="photo"></div>
+                            <button className="button--rectangle-secondary-middle-size lrbuton icon-exit--white">
+                                <p>Sair</p>
+                            </button>
+                        </div>
+                    </div>
+                
+                    {/* Main Content */}
+                    <div className="container--main-content">
+                        
+                        {/* Sidebar */}
+                        <div className="container--sidebar">
+                            <div className="sidebar" id="box">
+                                <button 
+                                    className="icon--home-white selected"                                
+                                    onClick={() => accessPage("home")}
+                                >
+                                    <span>Início</span>
+                                </button>
 
-                    <button 
-                        className="button--sidebar-retract" 
-                        onClick={animateSidebar}
-                    >
-                    </button>                    
-                                        
-                    {/* Dynamic Content - Miolo */}
-                    <Resume />
+                                <button 
+                                    className="icon--agendamentos"
+                                    onClick={() => accessPage("agenda")}
+                                >
+                                    <span>Agenda</span>
+                                </button>
+
+                                <button 
+                                    className="icon--pacientes"
+                                    onClick={() => accessPage("pacientes")}
+                                >
+                                    <span>Pacientes</span>
+                                </button>
+                                
+                                <button 
+                                    className="icon--financas"
+                                    onClick={() => accessPage("financas")}
+                                >
+                                    <span>Financeiro</span>
+                                </button>
+                                
+                                <button 
+                                    className="icon--exams"
+                                    onClick={() => accessPage("exams")}
+                                >
+                                    <span>Exames</span>
+                                </button>
+
+                                <button 
+                                    className="icon--procedure"
+                                    onClick={() => accessPage("tratamentos")}
+                                >       
+                                    <span>Tratamentos</span>
+                                </button>
+                                
+                                <button 
+                                    className="icon--laboratory"
+                                    onClick={() => accessPage("laboratorio")}
+                                >
+                                    <span>Laboratório</span>
+                                </button>
+                                
+                                <button 
+                                    className="icon--news"
+                                    onClick={() => accessPage("noticias")}
+                                >
+                                    <span>Notícias</span>
+                                </button>                            
+
+                                <button 
+                                    className="icon--help"
+                                    onClick={() => this.teste("ajuda")}
+                                >
+                                    <span>Ajuda</span>
+                                </button>
+                                
+                                <button 
+                                    className="icon--nubibuz"
+                                    onClick={() => accessPage("nubibuz")}
+                                >
+                                    <span>Sobre Nós</span>
+                                </button>                   
+                            </div>
+                        </div>        
+
+                        <button 
+                            className="button--sidebar-retract" 
+                            onClick={animateSidebar}
+                        >
+                        </button>
+
+                        <p>{this.testeStr}</p>
+
+                        {/* Dynamic Content - Miolo */}
+                        <div id="dynamicContent">
+                            { React.createElement(this.state.child) }
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
+
+export default Main;
