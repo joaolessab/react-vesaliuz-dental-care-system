31e55ec87981ffdfd4ecf34ae15cd09e62f4f473
João Vitor Lessa <joaovitorlessa@gmail.com>
2020-03-29 21:05:52 -0300

    Help page models

--- frontend/src/components/Help.js
+++ frontend/src/components/Help.js
@@ -15,6 +15,35 @@ export default function Help(){
 
     // RETORNO BÁSICO DO HTML
     return (
-        <p>Oi</p>
+        <div className="container--miolo">
+            <div className="container--content">
+                <div className="div--title">
+                    <h1>Como nós podemos te ajudar?</h1>
+                </div>
+
+                <div className="div--info">
+                    <div className="div--info-icon">
+                    </div>
+                    <div className="div--info-text">
+                        <h1>Aprenda como organizar sua agenda</h1>
+                        <p>Seus rendimentos dependem do seu tempo disponível. Quanto mais você escalar o seu negócio, mais resultados irá obter. Esse artigo aborda exatamente o passo a passo para...</p>
+                    </div>
+                </div>
+
+                <div className="div--chart">
+                    <div className="revenue">
+                        <h1>R$ 2.730,50</h1>
+                        <p>Opa, lucrou? Confira sua receita</p>
+                    </div>
+                    <div className="expense">
+                        <h1>R$ 1.302,26</h1>
+                        <p>Gastou, né? Confira suas despesas</p>
+                    </div>
+                </div>
+            </div>
+
+            <div className="container--auxiliar">
+            </div>
+        </div>
     );
 }
