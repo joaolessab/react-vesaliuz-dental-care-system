f31612630eb942ac9e4724f6dd8a1129ba1344f6
João Vitor Lessa <joaovitorlessa@gmail.com>
2020-04-09 09:31:52 -0300

    Help sendo normalizado

--- frontend/src/layout--model/MODELO.js
+++ frontend/src/layout--model/MODELO.js
@@ -11,9 +11,9 @@ export default function Help(){
     // RETORNO BÁSICO DO HTML
     return (
         <div className="container--miolo-main">
-            <div className="container--content-help">
+            <div className="container--content-modelo">
                 <div className="div--content-title">
-                    <h1>Como nós podemos te ajudar?</h1>
+                    <h1>Página Inicial</h1>
                 </div>
 
                 <div className="div--chart">
@@ -34,27 +34,7 @@ export default function Help(){
                 </div>
             </div>
 
-            <div className="container--form-help">
-                <p className="p--subtitle">Conte-nos o seu problema</p>
-                <form>
-                    <label for="name">Seu nome:</label>
-                    <input type="text" id="name" placeholder="Digite o seu nome" className="input--white-context-wth-shadow"/>
-
-                    <label for="email">Seu e-mail:</label>
-                    <input type="email" id="email" placeholder="Digite seu melhor e-mail" className="input--white-context-wth-shadow"/>
-
-                    <label for="telephone">Seu telefone:</label>
-                    <input type="text" id="telephone" placeholder="Digite o seu melhor telefone" className="input--white-context-wth-shadow"/>
-
-                    <label for="situation">Seu problema:</label>
-                    <textarea id="situation" placeholder="Digite o seu relato" className="input--white-context-wth-shadow">
-                    </textarea>
-
-                    <input type="submit" value="Enviar e-mail" className="button--white-context"></input>                            
-                </form>
-            </div>
-
-            <div className="container-questions--help">
+            <div className="container--auxiliar-resume">
                 <div className="div--topbuttons">
                     <button className="button--grey-label_underlined selected">Dia</button>
                     <button className="button--grey-label_underlined">Semana</button>
