d4d426305d9c2f4f93c692076ca823e20555738d
João Vitor Lessa <joaovitorlessa@gmail.com>
2020-04-12 04:15:47 -0300

    Notícias hover button

--- frontend/src/components/News.js
+++ frontend/src/components/News.js
@@ -40,8 +40,9 @@ export default function News(){
                         <div className="div--item-news-text">
                             <h1>COVID-19: Você já se preparou para combater o prejuízo do seu consultório?</h1>
                             <p>Tempos difíceis chegaram. Estamos enfrentando uma crise sem previsão de terminar. Não há estabilidade nesse cenário, porém, você está preparado para enfrentar? Ou está sentado esperando tudo acontecer? Confira aqui nossas dicas.</p>
-                            <div>
-                                <button>Ler mais...</button>
+                            <div className="div--details">
+                                <p>Ler mais</p>
+                                <button></button>
                             </div>
                         </div>
                     </div>
@@ -55,8 +56,9 @@ export default function News(){
                         <div className="div--item-news-text">
                             <h1>Novas cadeiras disponíveis no mercado</h1>
                             <p>As novas cadeiras com suspensão a ar e flexibilidade rotativa estão dando o que falar no mercado odontológico. A mais nova inovação foi criada e patenteada pela empresa "Sony" e seus preços já estão disponíveis no mercado em torno de R$ 5.000,00.</p>
-                            <div>
-                                <button>Ler mais...</button>
+                            <div className="div--details">
+                                <p>Ler mais</p>
+                                <button></button>
                             </div>
                         </div>
                     </div>
@@ -70,8 +72,9 @@ export default function News(){
                         <div className="div--item-news-text">
                             <h1>Investimentos: agora é hora de reeinvestir no seu negócio?</h1>
                             <p>A resposta é simples e direta: você possui caixa? Se preparou para momentos difíceis e de instabilidade? Se a resposta for sim: com certeza deve continuar reinvestindo no seu negócio. Caso a resposta seja não: agora é o momento de mantér o fluxo de caixa do seu negócio.</p>
-                            <div>
-                                <button>Ler mais...</button>
+                            <div className="div--details">
+                                <p>Ler mais</p>
+                                <button></button>
                             </div>
                         </div>
                     </div>
