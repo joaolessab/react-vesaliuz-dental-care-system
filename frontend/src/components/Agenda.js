b34446c9f70b3423b3db4b6703e4f25aa19c35df
João Vitor Lessa <joaovitorlessa@gmail.com>
2020-04-21 02:07:57 -0300

    Teste da primeira agenda

--- frontend/src/components/Agenda.js
+++ frontend/src/components/Agenda.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import 'react-responsive-modal/styles.css';
 import { Modal } from 'react-responsive-modal';
+import Paper from '@material-ui/core/Paper';
+import { ViewState } from '@devexpress/dx-react-scheduler';
+import {
+  Scheduler,
+  DayView,
+  Appointments,
+} from '@devexpress/dx-react-scheduler-material-ui';
 
 // ARQUIVOS CSS E IMAGENS DEVEM SER IMPORTADOS AQUI
 import '../assets/css/Agenda.css';
@@ -12,9 +19,11 @@ class Agenda extends React.Component{
 
         /* VARIABLES */
         this.state = {
-            modalVisibility: false,
-            newsFilter: 10,
-            selectedTags: ["Todos"]
+            currentDate : '2018-11-01',
+            schedulerData : [
+                { startDate: '2018-11-01T09:45', endDate: '2018-11-01T11:00', title: 'Meeting' },
+                { startDate: '2018-11-01T12:00', endDate: '2018-11-01T13:30', title: 'Go to a gym' },
+            ]
         };
         
         this.modalTitle = "";
@@ -33,7 +42,22 @@ class Agenda extends React.Component{
                         <h1>Agenda</h1>
                     </div>
 
-                    <p>Testando</p>
+                    <div className="div--content-agenda">
+                    <Paper>
+                        <Scheduler
+                        data={this.state.schedulerData}
+                        >
+                        <ViewState
+                            currentDate={this.state.currentDate}
+                        />
+                        <DayView
+                            startDayHour={9}
+                            endDayHour={14}
+                        />
+                        <Appointments />
+                        </Scheduler>
+                    </Paper>
+                    </div>
                 </div>
 
                 {/* Modal de notícia */}
