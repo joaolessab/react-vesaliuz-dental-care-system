3642164ef8a5599618996f54462befee9f26a0c5
João Vitor Lessa <joaovitorlessa@gmail.com>
2020-04-28 17:27:41 -0300

    Big Calendar

--- frontend/src/components/Agenda.js
+++ frontend/src/components/Agenda.js
@@ -1,116 +1,13 @@
 import React from 'react';
 import 'react-responsive-modal/styles.css';
 import { Modal } from 'react-responsive-modal';
-import Paper from '@material-ui/core/Paper';
-import { ViewState, EditingState } from '@devexpress/dx-react-scheduler';
-import {
-  AllDayPanel,
-  Scheduler,
-  Resources,
-  MonthView,
-  DayView,
-  WeekView,
-  DragDropProvider,
-  DateNavigator,
-  Toolbar,
-  TodayButton,
-  Appointments,
-  AppointmentTooltip,
-  AppointmentForm,
-  EditRecurrenceMenu,
-  ViewSwitcher
-} from '@devexpress/dx-react-scheduler-material-ui';
-import { owners } from "../mock--API/agenda/tasks";
-import { appointments, resourcesData } from "../mock--API/agenda/resources";
-/*import MenuItem from '@material-ui/core/MenuItem';
-import { withStyles } from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';*/
+
+import { Calendar, momentLocalizer } from 'react-big-calendar'
+import moment from 'moment';
 
 // ARQUIVOS CSS E IMAGENS DEVEM SER IMPORTADOS AQUI
 import '../assets/css/Agenda.css';
-
-const allLocalizationMessages = {
-    'fr-FR': {
-      allDay: 'Temps plein',
-    },
-    'de-GR': {
-      allDay: 'Ganztägig',
-    },
-    'en-US': {
-      allDay: 'All Day',
-      today: 'Today'
-    },
-    'pt-BR': {
-        allDay: 'Dia todo',
-        today: 'Hoje',
-
-        /*AppointmentForm*/
-        detailsLabel : 'Detalhes',
-        allDayLabel : 'O dia todo',
-        titleLabel : 'Título',
-        commitCommand : 'Salvar',
-        moreInformationLabel : 'Mais informações',
-        repeatLabel : 'Repetir',
-        notesLabel : 'Escreva aqui mais detalhes',
-        never : 'Nunca',
-        daily : 'Diariamente',
-        weekly : 'Semanalmente',
-        monthly : 'Mensalmente',
-        yearly : 'Anualmente',
-        repeatEveryLabel : 'Repetir a cada',
-        daysLabel : 'dia(s)',
-        endRepeatLabel : 'Parar repetição',
-        onLabel : 'Em',
-        afterLabel : 'Depois de',
-        occurrencesLabel : 'ocorrência(s)',
-        weeksOnLabel : 'semana(s)',
-        monthsLabel : 'mês(es)',
-        ofEveryMonthLabel : 'de todos mês',
-        theLabel : 'O',
-        firstLabel : 'Primeiro',
-        secondLabel : 'Segundo',
-        thirdLabel : 'Terceiro',
-        fourthLabel : 'Quarto',
-        lastLabel : 'Quinto',
-        yearsLabel : 'ano(s)',
-        ofLabel : '',
-        everyLabel : 'Em'
-    }
-};
-
-const getLocalizationMessages = locale => allLocalizationMessages[locale];
-
-/*const styles = theme => ({
-    container: {
-        display: 'flex',
-        marginBottom: theme.spacing(2),
-        justifyContent: 'flex-end',
-    },
-    text: {
-        ...theme.typography.h6,
-        marginRight: theme.spacing(2),
-    },
-});*/
-
-/*const LocaleSwitcher = withStyles(styles, { name: 'LocaleSwitcher' })(
-    ({ onLocaleChange, currentLocale, classes }) => (
-        <div className={classes.container}>
-            <div className={classes.text}>
-                Locale:
-            </div>
-            <TextField
-                select
-                value={currentLocale}
-                onChange={onLocaleChange}
-            >
-                <MenuItem value="fr-FR">Le français (French)</MenuItem>
-                <MenuItem value="de-GR">Deutsch (German)</MenuItem>
-                <MenuItem value="en-US">English (United States)</MenuItem>
-                <MenuItem value="pt-BR">Portuguese (Brazil)</MenuItem>
-            </TextField>
-        </div>
-    ),
-);*/
+import 'react-big-calendar/lib/css/react-big-calendar.css';
 
 class Agenda extends React.Component{
 
@@ -118,60 +15,18 @@ class Agenda extends React.Component{
         super(props);
 
         /* VARIABLES */
-        this.state = {
-            data: appointments,
-            resources: [
-                {
-                    fieldName: 'roomId',
-                    title: 'Sala',
-                    instances: resourcesData,
-                },
-                {
-                    fieldName: 'members',
-                    title: 'Membros',
-                    instances: owners,
-                    allowMultiple: true,
-                },
-            ],
-            locale: 'pt-BR'
-        };
+        this.state = { };
         
         this.modalTitle = "";
         this.modalBody = "";
-        this.modalPicture = "";
-        
-        this.openEdit = true;
-        this.commitChanges = this.commitChanges.bind(this);
+        this.modalPicture = "";        
     }
-
-    teste = (event) => {
-        debugger
-    };
-
-    commitChanges = ({ added, changed, deleted }) => {
-        debugger
-        this.setState((state) => {
-          let { data } = state;
-          if (added) {
-            const startingAddedId = data.length > 0 ? data[data.length - 1].id + 1 : 0;
-            data = [...data, { id: startingAddedId, ...added }];
-          }
-          if (changed) {
-            data = data.map(appointment => (
-              changed[appointment.id] ? { ...appointment, ...changed[appointment.id] } : appointment));
-          }
-          if (deleted !== undefined) {
-            data = data.filter(appointment => appointment.id !== deleted);
-          }
-          return { data };
-        });
-    };
-
-    changeLocale = event => this.setState({ locale: event.target.value });
 
     // Visualização de Todo o conteúdo do HTML
     render(){
-        const { data, resources, locale, currentDate } = this.state;
+        const localizer = momentLocalizer(moment);
+
+        const myEventsList = [];
 
         // RETORNO BÁSICO DO HTML
         return (
@@ -182,77 +37,13 @@ class Agenda extends React.Component{
                     </div>
 
                     <div className="div--content-agenda">
-                        {/* 
-                        <LocaleSwitcher
-                            currentLocale={ locale }
-                            onLocaleChange={ this.changeLocale }
-                        />
-                        */}
-                        <Paper>
-                            <Scheduler
-                                data={ data }
-                                locale={ locale }
-                            >
-                            <ViewState
-                                defaultCurrentDate="2017-05-25"
-                            />
-                            {/* 
-                            <ViewState
-                                defaultCurrentDate={currentDate}
-                            />
-                            */}
-                            <EditingState
-                                onCommitChanges={ this.commitChanges }
-                            />
-                            <EditRecurrenceMenu />
-
-                            <WeekView
-                                startDayHour={8}
-                                endDayHour={20}
-                                displayName="Semana"
-                            />
-                            <WeekView
-                                name="work-week"
-                                displayName="Semana (Dias Úteis)"
-                                excludedDays={[0, 6]}
-                                startDayHour={8}
-                                endDayHour={20}
-                            />
-                            <MonthView
-                                displayName="Mês"
-                            />
-                            <DayView
-                                displayName="Dia"
-                                startDayHour={8}
-                                endDayHour={20}
-                            />
-
-                            <Appointments />
-                            <AppointmentTooltip
-                                showOpenButton                                
-                                showDeleteButton
-                            />
-
-                            <AppointmentForm
-                                messages={ getLocalizationMessages(locale) }
-                            />
-
-                            <Resources
-                                data={ resources }
-                                mainResourceName="roomId"
-                            />
-                            <Toolbar />
-                            <ViewSwitcher />
-                            <DateNavigator />
-                            <AllDayPanel
-                                messages={ getLocalizationMessages(locale) }
-                            />                            
-                            <DragDropProvider />
-                            <TodayButton
-                                messages={ getLocalizationMessages(locale) }
-                            />
-                            </Scheduler>
-                        </Paper>
+                    <Calendar
+                        localizer={ localizer }
+                        events={ myEventsList }
+                        startAccessor="start"
+                        endAccessor="end"
+                        style={{ height: 500 }}
+                    />
                     </div>
                 </div>
 
