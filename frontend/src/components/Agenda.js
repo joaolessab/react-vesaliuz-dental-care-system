532c484b561593cae805e67370506ba4cb38d7cc
João Vitor Lessa <joaovitorlessa@gmail.com>
2020-05-02 21:15:05 -0300

    Problemas com moment

--- frontend/src/components/Agenda.js
+++ frontend/src/components/Agenda.js
@@ -1,11 +1,16 @@
 import React, { createElement } from 'react';
 import 'react-responsive-modal/styles.css';
 import { Modal } from 'react-responsive-modal';
+import Button from '@material-ui/core/Button';
+import NavigateNextIcon from '@material-ui/icons/NavigateNext';
+import NavigateBeforeIcon from '@material-ui/icons/NavigateBefore';
+import TodayIcon from '@material-ui/icons/Today';
+import AddBoxIcon from '@material-ui/icons/AddBox';
+import Tooltip from '@material-ui/core/Tooltip';
+import Zoom from '@material-ui/core/Zoom';
 
 import { Calendar, momentLocalizer } from 'react-big-calendar'
-import moment from 'moment'
-import 'moment/locale/pt-br';
-
+import moment from 'moment';
 import withDragAndDrop from 'react-big-calendar/lib/addons/dragAndDrop';
 
 // ARQUIVOS CSS E IMAGENS DEVEM SER IMPORTADOS AQUI
@@ -21,13 +26,15 @@ class Agenda extends React.Component{
 
         /* VARIABLES */
         this.state = {
+            dateChosen: moment().toDate(),
             events: [
                 {
                     start: moment().toDate(),
                     end: moment().add(1, "days").toDate(),
                     title: "Teste do João"
                 }
-            ]
+            ],
+            title: "Maio de 2020"
         };
          
         this.viewType = "month";
@@ -46,9 +53,67 @@ class Agenda extends React.Component{
         this.insertScrollAgendaDiv();
     }*/
 
-    changeAgendaType = (newView) => {
-        this.viewType = newView;
+    changeAgendaType = (newType) => {
+        this.findLiteralDate(newType);
+        this.viewType = newType;
         this.forceUpdate();
+    };
+
+    findLiteralDate = (newType) => {
+        if (newType == "day"){
+            var oi = this.state.dateChosen.day();
+            
+            debugger
+            var day = this.state.dateChosen.getDate();
+            var month = this.findMonthString(this.state.dateChosen.getMonth().toString());
+            var year = this.state.dateChosen.getFullYear();
+
+            this.state.title = day + " de " + month + " de " + year;
+        }
+        else if (newType == "week"){
+            debugger
+        } 
+        else if (newType == "month"){
+            debugger
+            var month = this.findMonthString(this.state.dateChosen.getMonth().toString()).toLocaleLowerCase();
+            var year = this.state.dateChosen.getFullYear();
+
+            this.state.title = month + " de " + year;
+        }        
+        else if (newType == "agenda"){
+            this.state.title = "Inicio e Fim dos eventos";
+        }
+    };
+
+    findMonthString = (month) => {
+        switch(month) {
+            case "1":
+                return "Janeiro";
+            case "2":
+                return "Fevereiro";
+            case "3":
+                return "Março";
+            case "4":
+                return "Abril";
+            case "5":
+                return "Maio";
+            case "6":
+                return "Junho";
+            case "7":
+                return "Julho";
+            case "8":
+                return "Agosto";
+            case "9":
+                return "Setembro";
+            case "10":
+                return "Outubro";
+            case "11":
+                return "Novembro";
+            case "12":
+                return "Dezembro";
+            default:
+                return '-';
+        }
     };
 
     onEventResize = (type, { event, start, end, allDay }) => {
@@ -99,22 +164,31 @@ class Agenda extends React.Component{
                     <div className="div--content-agenda">
                         <div className="bigcalendar--toolbar-custom">
                             <div className="div--days">
-                                <button>Anterior</button>
-                                <button>Hoje</button>
-                                <button>Próximo</button>
+                                <Tooltip TransitionComponent={Zoom} placement="bottom" title="Anterior">
+                                    <Button><NavigateBeforeIcon/></Button>
+                                </Tooltip>
+                                <Tooltip TransitionComponent={Zoom} placement="bottom" title="Hoje">
+                                    <Button><TodayIcon/></Button>
+                                </Tooltip>
+                                <Tooltip TransitionComponent={Zoom} placement="bottom" title="Próximo">
+                                    <Button><NavigateNextIcon/></Button>
+                                </Tooltip>
+                                <Tooltip TransitionComponent={Zoom} placement="bottom" title="Novo evento">
+                                    <Button><AddBoxIcon/></Button>
+                                </Tooltip>
                             </div>
                             <div className="text--date">
-                                <p>Maio de 2020</p>
+                                <p>{ this.state.title }</p>
                             </div>
                             <div className="div--view">
-                                <button onClick={() => this.changeAgendaType("month")}>Mês</button>
-                                <button onClick={() => this.changeAgendaType("week")}>Semana</button>
                                 <button onClick={() => this.changeAgendaType("day")}>Dia</button>
+                                <button onClick={() => this.changeAgendaType("week")}>Semana</button>
+                                <button onClick={() => this.changeAgendaType("month")} className="selected">Mês</button>
                                 <button onClick={() => this.changeAgendaType("agenda")}>Agenda</button>
                             </div>
                         </div>
                         <DnDCalendar
-                            defaultDate={moment().toDate()}
+                            defaultDate={ this.state.dateChosen }
                             defaultView= { this.viewType }
                             localizer={ localizer }
                             startAccessor="start"
