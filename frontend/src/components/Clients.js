bd118c3622fc6aa8ac13952d773ec4165b7370c5
João Vitor Lessa <joao.barros@fatec.sp.gov.br>
2020-06-21 19:18:50 -0300

    [Clients] - Cards mais visuais

--- frontend/src/components/Clients.js
+++ frontend/src/components/Clients.js
@@ -25,24 +25,28 @@ class Clients extends React.Component{
                             <div className="div--card-picture clark"></div>
                             <div className="div--card-name">Clark Cold</div>
                             <div className="div--card-contact">clarkcold@gmail.com<br/>(12) 99088-4140</div>
+                            <div className="div--card-address">Avenia Brasil, 21</div>
                         </div>
                         
                         <div className="div--individual-card">
                             <div className="div--card-picture diana"></div>
                             <div className="div--card-name">Diana Mendes</div>
                             <div className="div--card-contact">dianamendes@gmail.com<br/>(12) 87995-1105</div>
+                            <div className="div--card-address">Rua Monsenhor Dutra, 43</div>
                         </div>
                         
                         <div className="div--individual-card">
                             <div className="div--card-picture oliver"></div>
                             <div className="div--card-name">Oliver Queen</div>
                             <div className="div--card-contact">oliverthekey@gmail.com<br/>(12) 97865-2141</div>
+                            <div className="div--card-address">Avenida JK, 110</div>
                         </div>  
 
                         <div className="div--individual-card">
                             <div className="div--card-picture bruce"></div>
                             <div className="div--card-name">Brunce Wayne da Silva</div>
                             <div className="div--card-contact">brucesilva@gmail.com<br/>(12) 99065-4040</div>
+                            <div className="div--card-address">Rua Jacareí, 230</div>
                         </div>                                              
                     </div>
                 </div>
