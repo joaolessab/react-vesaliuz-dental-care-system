444fd76fc14dd2707e4f345cd9dae58b07f0fe80
João Vitor Lessa <joao.barros@fatec.sp.gov.br>
2020-10-15 00:34:25 -0300

    [Finances] - Anexos ok

--- frontend/src/components/Upload.js
+++ frontend/src/components/Upload.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import '../assets/css/Upload.css';
-import Button from '@material-ui/core/Button';
+import Dropzone from "./Dropzone";
 
 class Upload extends React.Component {
-  constructor(){
-    super();
+  constructor(props){
+    super(props);
 
     this.state = {
         files: [],
@@ -13,25 +13,88 @@ class Upload extends React.Component {
         successfullUploaded: false
     };
   }
-  
-  render () {
-    //const { files } = this.state;
 
+  onFilesAdded = (files) => {
+    this.setState(prevState => ({
+      files: prevState.files.concat(files)
+    }));
+  }
+
+  /*async uploadFiles() {
+    this.setState({ uploadProgress: {}, uploading: true });
+    const promises = [];
+    this.state.files.forEach(file => {
+      promises.push(this.sendRequest(file));
+    });
+    try {
+      await Promise.all(promises);
+
+      this.setState({ successfullUploaded: true, uploading: false });
+    } catch (e) {
+      // Not Production ready! Do some error handling here instead...
+      this.setState({ successfullUploaded: true, uploading: false });
+    }
+  }*/
+
+  /*sendRequest(file) {
+    return new Promise((resolve, reject) => {
+      const req = new XMLHttpRequest();
+
+      req.upload.addEventListener("progress", event => {
+        if (event.lengthComputable) {
+          const copy = { ...this.state.uploadProgress };
+          copy[file.name] = {
+            state: "pending",
+            percentage: (event.loaded / event.total) * 100
+          };
+          this.setState({ uploadProgress: copy });
+        }
+      });
+
+      req.upload.addEventListener("load", event => {
+        const copy = { ...this.state.uploadProgress };
+        copy[file.name] = { state: "done", percentage: 100 };
+        this.setState({ uploadProgress: copy });
+        resolve(req.response);
+      });
+
+      req.upload.addEventListener("error", event => {
+        const copy = { ...this.state.uploadProgress };
+        copy[file.name] = { state: "error", percentage: 0 };
+        this.setState({ uploadProgress: copy });
+        reject(req.response);
+      });
+
+      const formData = new FormData();
+      formData.append("file", file, file.name);
+
+      req.open("POST", "http://localhost:8000/upload");
+      req.send(formData);
+    });
+  }*/
+
+  render () {
     return (
-        <div className="div--upload-container">
-            <div className="div--upload-upload">
-                <Button>Anexar</Button>
-            </div>
-            <div className="div--upload-files">
-                <div className="div--upload-fileItem">
-                    <p>Arquivo.png</p>
-                    <div className="div--upload-progressContainer">
-                        <div className="div--upload-progressBar"></div>
-                        <div className="div--upload-loader"></div>
-                    </div>
+        <div className="Upload">
+        <span className="Title">Upload Files</span>
+        <div className="Content">
+          <div>
+            <Dropzone
+              onFilesAdded={this.onFilesAdded}
+              disabled={this.state.uploading || this.state.successfullUploaded}
+            />
+          </div>
+          <div className="Files">
+            {this.state.files.map(file => {
+              return (
+                <div key={file.name} className="Row">
+                  <span className="Filename">{file.name}</span>
                 </div>
-            </div>
+              );
+            })}
+          </div>
         </div>
+      </div>
     );    
   }
 }
